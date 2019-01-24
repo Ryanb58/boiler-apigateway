7bf7b88ccc1871115c9ccd4973060184e7458929
Taylor Brazelton <taylor.r.brazelton@gmail.com>
2019-01-24 17:53:34 -0500

    Proper response type.

--- services/accounts.py
+++ services/accounts.py
@@ -1,9 +1,10 @@
 import os
-
+import json
 from flask import Flask, jsonify, request
 from flask_restful import Resource
 from flask_jwt_simple import jwt_required, create_jwt
 import grpc
+from google.protobuf.json_format import MessageToJson
 
 from protos import accounts_pb2
 from protos import accounts_pb2_grpc
@@ -11,20 +12,9 @@ from protos import accounts_pb2_grpc
 HOST = os.environ.get('ACCOUNT_SERVICE_HOST', '0.0.0.0')
 PORT = str(os.environ.get('ACCOUNT_SERVICE_PORT', '22222'))
 
-class User(object):
-    def __init__(self, id, email, name):
-        self.id = id
-        self.email = email
-        self.name = name
-
-    def __str__(self):
-        return "User(id={}, email={}, name={})".format(self.id, self.email, self.name)
-
-    def identity(payload):
-        user_id = payload['identity']
-        return {"user_id": user_id}
 
 class AuthenticationResource(Resource):
+
     def post(self):
         # Provide a method to create access tokens. The create_jwt()
         # function is used to actually generate the token
@@ -41,20 +31,16 @@ class AuthenticationResource(Resource):
             return jsonify({"msg": "Missing password parameter"})
 
         # Verify the email/password.
-
-        with grpc.insecure_channel(host + ':' + port) as channel:
+        with grpc.insecure_channel(HOST + ':' + PORT) as channel:
             stub = accounts_pb2_grpc.AccountServiceStub(channel)
             try:
-                authenticated = stub.AuthenticateByEmail(
+                user = stub.AuthenticateByEmail(
                     accounts_pb2.AuthenticateByEmailRequest(email=email, password=password))
             except Exception as e:
                 print("Invalid username or password? Or perphaps we couldn't connect?")
+                print(e)
                 return False
 
-        if authenticated:
-            return User(id=authenticated.id)
-
-        user = verify(email, password)
         if not user:
             return jsonify({"msg": "Bad email or password"})
 
@@ -64,13 +50,15 @@ class AuthenticationResource(Resource):
 
 
 class AccountResource(Resource):
+
     @jwt_required
     def get(self):
-        with grpc.insecure_channel(host + ':' + port) as channel:
+        with grpc.insecure_channel(HOST + ':' + PORT) as channel:
             stub = accounts_pb2_grpc.AccountServiceStub(channel)
             try:
-                authenticated = stub.AuthenticateByEmail(
-                    accounts_pb2.AuthenticateByEmailRequest(email=email, password=password))
+                accounts = stub.List(accounts_pb2.ListAccountsRequest(page_size=1, page_token="1"))
             except Exception as e:
                 print("Invalid username or password? Or perphaps we couldn't connect?")
+                print(e)
                 return False
+            return jsonify(json.loads(MessageToJson(accounts)))
